711682e008a67930fe0d52da3b0a101f44f23eaa
JeanFrancoisGay@gmail.com <JeanFrancoisGay@gmail.com>
2025-03-28 12:17:13 -0400

    Adding a test controller

---
+++ FunDoo_API/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using FunDoo_API.DAL;
+using FunDoo_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunDoo_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            try
+            {
+                return "Success";
+            }
+            catch
+            {
+                return StatusCode(500, "Failure!");
+            }
+        }
+    }
+}
